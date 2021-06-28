c5444be35d2ae59560fad2e8109602917d19f45f
Leon Valencia <techie@lynx.rocks>
2021-06-29 05:44:06 +0800

    configure flink app

--- lib/stream-stack.ts
+++ lib/stream-stack.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
 import { Stream } from '@aws-cdk/aws-kinesis';
-import { Application, ApplicationCode, Runtime } from '@aws-cdk/aws-kinesisanalytics-flink';
+import { Application, ApplicationCode, Runtime, PropertyGroups } from '@aws-cdk/aws-kinesisanalytics-flink';
 
 export interface StreamProps extends StackProps {}
 
@@ -11,10 +11,27 @@ export class StreamStack extends Stack {
     super(scope, id, streamProps);
     const inStream = new Stream(this, "InStream");
     const outStream = new Stream(this, "OutStream");
-    const code = ApplicationCode.fromAsset(join(__dirname, 'flink-processor'))
+    const code = ApplicationCode.fromAsset(join(__dirname, 'flink-processor'));
+    const propertyGroups = {
+      'kinesis.analytics.flink.run.options': {
+        python: 'index.py',
+        jarfile: 'lib/kinesis-sql-connector.jar',
+      },
+      'consumer.config.0': {
+        'input.stream.name': inStream.streamName,
+        'aws.region': this.region,
+        'flink.stream.initpos': 'LATEST',
+      },
+      'producer.config.0': {
+        'output.stream.name': outStream.streamName,
+        'aws.region': this.region,
+        'shard.count': '1',
+      },
+    };
     const flinkApp = new Application(this, 'FlinkApp', {
       code,
       runtime: Runtime.FLINK_1_11,
+      propertyGroups,
     });
     inStream.grantRead(flinkApp);
     outStream.grantWrite(flinkApp);
